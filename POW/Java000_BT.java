ad4c44428fa6cf81439aa999a57bc7b3aff64cda
Girlgamer12427 <elangeloscuro1@gmail.com>
2018-09-01 10:39:44 -0700

    Java000_BT is complete

--- POW/Java000_BT.java
+++ POW/Java000_BT.java
@@ -1,14 +1,13 @@
 
 import java.util.Scanner;
 
-public class JavaTest
+public class Java000_BT
 {
 	public static void main(String[] args)
 	{	
-		
 		Scanner keyboard = new Scanner(System.in);
 		
-		System.out.println("Hello World");
+		System.out.println("\\\" Hello \n World!\"\\");
 		
 		keyboard.close();
 	}

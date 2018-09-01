3ee9b6c0d3aae854d7859e7c1d56e7c6da7a9af3
Girlgamer12427 <elangeloscuro1@gmail.com>
2018-09-01 07:03:16 -0700

    This changed

---
+++ POW/Java000_BT.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class JavaTest
+{
+	public static void main(String[] args)
+	{	
+		
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.println("Hello World");
+		
+		keyboard.close();
+	}
+
+}
